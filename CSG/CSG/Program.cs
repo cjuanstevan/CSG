ea5ab295afc7f77c47a0cc7da05db04381ac31b9
cjuanstevan <cjuanstevan@gmail.com>
2020-03-18 16:42:47 -0500

    Update program.cs
    Se actualiza run y omite Database.cs

--- CSG/CSG/Program.cs
+++ CSG/CSG/Program.cs
@@ -16,7 +16,7 @@ namespace CSG
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Servicio_Tecnico());
         }
     }
 }
